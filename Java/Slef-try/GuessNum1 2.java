cd515bd857193e14ae979bceaaa04591f18a1e13
Chenpeel <3488944232@qq.com>
2023-03-13 19:20:56 +0800

    first

--- Java/Slef-try/GuessNum1 2.java
+++
@@ -1,5 +0,0 @@
-public class GuessNum1 {
-    public static void main(String[] args){
-        
-    }
-}
--- Java/Slef-try/PrimeNums 2.java
+++
@@ -1,5 +0,0 @@
-public class PrimeNums {
-    public static void main(String[] args) {
-        
-    }
-}
