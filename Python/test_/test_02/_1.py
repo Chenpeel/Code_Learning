01ecc3080dac412a87367281b6cfd70f197c1e6d
Chenpeel <3488944232@qq.com>
2023-03-13 19:06:27 +0800

    sirst

---
+++ Python/test_/test_02/_1.py
@@ -0,0 +1,4 @@
+s = 'a\nb\tc' 
+print(" {}".format(len(s)))
+print(0xA+0xB)
+print("{}".format(eval('2+4/5')))
---
+++ Python/test_/test_02/square_draw.py
@@ -0,0 +1,15 @@
+from turtle import *
+home()
+seth(90)
+ht()
+speed(10)
+pencolor("red")
+pensize(3)
+pendown()
+wid = 5
+for i in range(99):
+    fd(wid)
+    left(90)
+    wid+=5
+    
+done()
