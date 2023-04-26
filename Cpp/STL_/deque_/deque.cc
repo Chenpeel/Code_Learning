9256148ae725149a53aff9cd0ec410364e0e8be5
Chenpeel <alpha@ChenpeeldeMacBook-Air.local>
2023-04-26 13:11:58 +0800

    for long time

--- Cpp/STL_/deque_/deque.cc
+++ Cpp/STL_/deque_/deque.cc
@@ -10,35 +10,34 @@ void print(const deque<int> d) {
     cout << *it << " ";
   }
   cout << endl;
-  
 }
 
-void print(const vector<int>v) {
+void print(const vector<int> v) {
   // const deque必须使用const_iterator
   for (vector<int>::const_iterator it = v.begin(); it != v.end(); it++) {
     cout << *it << " ";
   }
-  cout << endl;
+  F cout << endl;
 }
 /**
- * @brief 
+ * @brief
  * deque just like vector,but there's a central controller to control the
  * address deque do not have the capacity
-*/
+ */
 
 void test() {
   deque<int> d1;
-  for (int i= 0; i < 10; i++) {
-    d1.push_back(i); //add the last one
-    d1.push_front(i);//insert the first one
+  for (int i = 0; i < 10; i++) {
+    d1.push_back(i);   // add the last one
+    d1.push_front(i);  // insert the first one
   }
   vector<int> v1;
-  for (int i=0; i < 10; i++) {
+  for (int i = 0; i < 10; i++) {
     v1.push_back(d1[i]);
   }
   print(d1);
-  d1.pop_back(); //delete the last one
-  d1.pop_front();//delete the first one
+  d1.pop_back();   // delete the last one
+  d1.pop_front();  // delete the first one
   print(d1);
 
   if (d1.empty()) {
@@ -46,7 +45,7 @@ void test() {
     cout << d1.size() << endl;
   }
   // resize
-//   d1.resize(15,0);
+  //   d1.resize(15,0);
   print(d1);
   print(v1);
 }
